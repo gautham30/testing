47835740038a8a339f508fa78791d2fd64655266
gowtham <awslearningforlife@gmail.com>
2023-02-14 15:36:50 +0530

    added new file

---
+++ testpath.ps1
@@ -0,0 +1,8 @@
+﻿If (Test-Path -Path "D:\temp\Test Folder\gauti.txt")
+{
+Copy-Item -Path "D:\temp\Test Folder\gauti.txt" -Destination (New-Item -Path "D:\testing\test21\" -ItemType Directory)
+Write-Host "files copied"
+}
+else{
+Write-Host "file does not exits"
+}
